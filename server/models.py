434ae9619ba2bfa3201523cd71a436e4a4626d64
Phoebe Oigara <phoebeoigara@gmail.com>
2023-06-20 13:35:45 +0300

    Created my models , and migrations for the sepcific tables

--- server/models.py
+++ server/models.py
@@ -11,13 +11,30 @@ class Zookeeper(db.Model):
     __tablename__ = 'zookeepers'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    birthday = db.Column(db.Date, nullable=False)
+
+    # Define the relationship between Zookeeper and Animal
+    animals = db.relationship('Animal', backref='zookeeper')
 
 class Enclosure(db.Model):
     __tablename__ = 'enclosures'
 
     id = db.Column(db.Integer, primary_key=True)
+    environment = db.Column(db.String(100), nullable=False)
+    open_to_visitors = db.Column(db.Boolean, default=False)
+
+    # Define the relationship between Enclosure and Animal
+    animals = db.relationship('Animal', backref='enclosure')
+
 
 class Animal(db.Model):
     __tablename__ = 'animals'
 
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(100), nullable=False)
+    species = db.Column(db.String(100), nullable=False)
+
+    # Define the foreign keys for the relationships
+    zookeeper_id = db.Column(db.Integer, db.ForeignKey('zookeepers.id'))
+    enclosure_id = db.Column(db.Integer, db.ForeignKey('enclosures.id'))
